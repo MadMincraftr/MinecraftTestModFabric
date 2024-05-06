307dba1af02d9a64e03bec90a87dc1ffb56275e4
MadMincraftr <37884292+MadMincraftr@users.noreply.github.com>
2024-05-06 15:45:17 -0500

    fix

--- src/main/java/com/pinktarr/mixin/MobBorderClampPatch.java
+++ src/main/java/com/pinktarr/mixin/MobBorderClampPatch.java
@@ -4,9 +4,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
-@Mixin(Entity.class)
+//@Mixin(LivingEntity.class)
 public class MobBorderClampPatch {
 	@Redirect(method = "tick", at = @At(target = "Lnet/minecraft/util/math/MathHelper;clamp(DDD)D", value = "INVOKE"))
 	private double redirectClamp(double value, double min, double max) {
--- src/main/java/com/pinktarr/mixin/PlayerBorderClampPatch.java
+++ src/main/java/com/pinktarr/mixin/PlayerBorderClampPatch.java
@@ -4,9 +4,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 
-@Mixin(ServerPlayerEntity.class)
+@Mixin(PlayerEntity.class)
 public class PlayerBorderClampPatch {
 	@Redirect(method = "tick", at = @At(target = "Lnet/minecraft/util/math/MathHelper;clamp(DDD)D", value = "INVOKE"))
 	private double redirectClamp(double value, double min, double max) {
